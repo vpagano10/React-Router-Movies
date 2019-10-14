a92b0c3b351cde2e85e542d77286aa18bcb48383
vpagano10 <vpagano10@gmail.com>
2019-10-14 18:08:34 -0400

    fixed issue with specific movie actors

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
     const id = props.match.params.movieID;
@@ -44,11 +44,11 @@ const Movie = (props) => {
         </div>
         <h3>Actors</h3>
 
-        {/* {stars.map(star => (
+        {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
-        ))} */}
+        ))}
       </div>
       <div className="save-button">Save</div>
     </div>
